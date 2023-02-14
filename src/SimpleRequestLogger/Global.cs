2a80abcec12b2909e99cc08af1400407094ac0b7
Yavor Fingarov <yavor.fingarov@outlook.com>
2023-02-14 09:34:22 +0200

    Fixed pipeline

--- src/SimpleRequestLogger/Global.cs
+++ src/SimpleRequestLogger/Global.cs
@@ -7,4 +7,3 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Performance", "CA1848:Use the LoggerMessage delegates", Justification = "Implementation-specific usage.")]
 [assembly: SuppressMessage("Usage", "CA2254:Template should be a static expression", Justification = "Implementation-specific usage.")]
-
